45161450e20f9ae7612599e2381276b4bf432ae6
Ayush Gupta <137040550+madmecodes@users.noreply.github.com>
2025-08-20 14:30:47 +0530

    disable chat temp

--- superchat/urls.py
+++ superchat/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-order/', views.create_order, name='create_order'),
-    path('razorpay-webhook/', views.razorpay_webhook, name='razorpay_webhook'),
-    path('get-super-chats/', views.get_super_chats, name='get_super_chats'),
+    # Temporarily disabled live chat endpoints
+    # path('create-order/', views.create_order, name='create_order'),
+    # path('razorpay-webhook/', views.razorpay_webhook, name='razorpay_webhook'),
+    # path('get-super-chats/', views.get_super_chats, name='get_super_chats'),
 ]
